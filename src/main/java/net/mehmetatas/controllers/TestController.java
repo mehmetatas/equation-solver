69bdda0e75a5554c9f2b4fdd8c35dce9ccca08a7
mehmet <mehmet@mehmetatas.net>
2016-04-20 19:59:57 +0300

    spring boot & hello world rest

---
+++ src/main/java/net/mehmetatas/controllers/TestController.java
@@ -0,0 +1,15 @@
+package net.mehmetatas.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by mehmet on 20.04.2016.
+ */
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
